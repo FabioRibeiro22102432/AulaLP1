4eeea8f56abbd78eb8553abb61fe439e91a804b1
Fábio Ribeiro <a22102432@alunos.ulht.pt>
2022-02-24 14:40:37 +0000

    Imprimir variaveis

--- Variaveis/Program.cs
+++ Variaveis/Program.cs
@@ -9,6 +9,9 @@ namespace Variaveis
             int inteiro;
             uint uinteiro;
             char letra;
+            float real;
+            double realDouble;
+            bool booleano;
 
 
             inteiro = 6; //inteiro
@@ -19,6 +22,17 @@ namespace Variaveis
             letra = 'a'; // char
 
             Console.WriteLine(letra);
+
+            real = 0.22222;
+            realDouble = 2.55555555;
+
+            Console.WriteLine("{0} {1}", real, realDouble);
+
+            booleano = false;
+
+            Console.WriteLine(booleano);
+
+
 
 
         }
