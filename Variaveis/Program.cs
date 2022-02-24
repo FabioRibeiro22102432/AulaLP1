9f517e7ac14e16918baa706226917b387d351750
Fábio Ribeiro <a22102432@alunos.ulht.pt>
2022-02-24 14:32:36 +0000

    Inteiros

---
+++ Variaveis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inteiro;
+            uint uinteiro;
+
+
+            inteiro = 6; //inteiro
+            uinteiro = 3; //inteiro unsigned
+            
+            Console.WriteLine("{0}, {1}", inteiro, uinteiro);
+
+
+        }
+    }
+}
