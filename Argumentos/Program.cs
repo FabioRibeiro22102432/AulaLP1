2142eb712f7a01062f756bc49dcf1191b6a98428
Fábio Ribeiro <a22102432@alunos.ulht.pt>
2022-02-24 13:55:38 +0000

    ciclo for

---
+++ Argumentos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for(i=0; args.leght > i; i++) 
+                {
+                     Console.WriteLine(args[i]);
+
+                }
+           
+        }
+    }
+}
