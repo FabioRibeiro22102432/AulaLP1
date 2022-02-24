d0da9f3267e952a101facc0727c93dee9ec72915
Fábio Ribeiro <a22102432@alunos.ulht.pt>
2022-02-24 14:57:14 +0000

    Imprimir frases

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase1 = "Um tab\t e um copyright\u03A9";
+            string frase2 = @"Um tab\t e um copyright\u03A9";
+            string frase3 = "\u000A";
+
+            Console.WriteLine(frase1);
+            Console.WriteLine(frase2);
+            Console.WriteLine(frase3);
+        }
+    }
+}
